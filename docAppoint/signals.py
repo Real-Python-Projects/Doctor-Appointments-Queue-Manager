6ef4fa71c16425f87a699c06f9a11df34af5c54c
Gateway47 <itsregalo047@gmail.com>
2021-05-20 17:53:47 +0300

    fixed celery tasks

---
+++ docAppoint/signals.py
@@ -0,0 +1,12 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from datetime import datetime, timedelta
+from .models import AppointmentSession
+
+@receiver(post_save, sender=WaitingPatients)
+def appointment_expiry(sender, instance, created, **kwargs):
+    if created:
+        AppointmentSession.apply_async(
+            args=(instance.pk),
+            eta=datetime.utcnow()+timedelta(minutes=30)
+        )
--- docAppoint/tasks.py
+++ docAppoint/tasks.py
@@ -2,16 +2,14 @@ from celery.decorators import task
 from celery.utils.log import get_task_logger
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
+from .models import AppointmentSession
+from celery import shared_task
 
 logger = get_task_logger(__name__)
 
-@periodic_task(
-    run_every=(crontab(minute='*/1')),
-    name="end_consultation",
-    ignore_result=True
-)
+@shared_task
 def end_patient_consultation(pk):
     try:
-        AppointmentSessions.objects.get(pk=0).delete()
+        AppointmentSessions.objects.get(pk=pk).delete()
     except AppointmentSessions.DoesNotExist:
         pass
