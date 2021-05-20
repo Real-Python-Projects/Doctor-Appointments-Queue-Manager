f1d20522a06e4962e86432ca24cafe115d0e9d4b
Gateway47 <itsregalo047@gmail.com>
2021-05-20 18:49:36 +0300

    done celery

--- docAppoint/tasks.py
+++ docAppoint/tasks.py
@@ -2,7 +2,7 @@ from celery.decorators import task
 from celery.utils.log import get_task_logger
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
-from .models import AppointmentSession
+from .models import AppointmentSession, WaitingPatients
 from celery import shared_task
 
 logger = get_task_logger(__name__)
@@ -15,4 +15,6 @@ logger = get_task_logger(__name__)
 def end_patient_consultation():
     model = AppointmentSession.objects.last()
     model.delete()
+    next_patient = WaitingPatients.objects.last()
+    AppointmentSession.objects.create(person_present=next_patient)
     return "Item Deleted"
